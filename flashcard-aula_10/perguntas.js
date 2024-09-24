bf0104512d007ef343f3e8d3894816c32b6f697d
LauraPereira2007 <165840254+LauraPereira2007@users.noreply.github.com>
2024-09-24 16:46:59 -0300

    Update perguntas.js

--- flashcard-aula_10/perguntas.js
+++ flashcard-aula_10/perguntas.js
@@ -21,3 +21,21 @@ criaCartao(
     'Como se diz cadeira em inglês?',
     'cadeira em ingles é chair'
 )
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Em quanto tempo aproximadamente, o plástico demora para se decompor',
+    '500 anos'
+)
+
+criaCartao(
+    'Celebridades',
+    'Quantos álbuns tem Taylor Swift?',
+    '11 álbuns'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz escritório em inglês?',
+    'escritório em ingles é desk'
+)
